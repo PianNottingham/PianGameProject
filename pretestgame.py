83f3d3ea2e1d3893ff4c4bac6844a58739c06cd7
PianNottingham <pian.nottingham@gmail.com>
2022-12-07 08:53:54 +0700

    Create pretestgame.py

---
+++ pretestgame.py
@@ -0,0 +1,15 @@
+#youtuber = "sutham pian" # some string variable
+
+# a few ways to do this
+#print ("subscribe to  " + youtuber)
+##print ("subscribe to {}".format(youtuber))
+#print (f"subscribe to {youtuber}")
+adj = input("adjective :")
+verb1   = input("verb1 :")
+verb2 = input("verb2 :")
+famous_person = input("famous_person :")
+
+madlib = f"computer programming is so {adj}! It makes me so excited all the time because \
+    i love to {verb1}. stay hydrated and {verb2} like you are {famous_person}!"  
+
+
