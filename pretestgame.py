05e7632d59a8bb3cbf607c870e8c2665caf09afe
PianNottingham <pian.nottingham@gmail.com>
2022-12-07 08:59:20 +0700

    Update pretestgame.py

--- pretestgame.py
+++ pretestgame.py
@@ -4,12 +4,16 @@
 #print ("subscribe to  " + youtuber)
 ##print ("subscribe to {}".format(youtuber))
 #print (f"subscribe to {youtuber}")
-adj = input("adjective :")
-verb1   = input("verb1 :")
-verb2 = input("verb2 :")
-famous_person = input("famous_person :")
 
-madlib = f"computer programming is so {adj}! It makes me so excited all the time because \
-    i love to {verb1}. stay hydrated and {verb2} like you are {famous_person}!"  
+#adj = input("adjective :")
+#verb1   = input("verb1 :")
+#verb2 = input("verb2 :")
+#famous_person = input("famous_person :")
+
+#madlib = f"computer programming is so {adj}! It makes me so excited all the time because \
+#i love to {verb1}. stay hydrated and {verb2} like you are {famous_person}!"  
+name = input("name :")
+print (f"you name is {name}!")
+
 
 
